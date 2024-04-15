549926c8ba7101c9eb372c625dcf585e77b176f0
Kishan Gupta <139811435+kishangithu@users.noreply.github.com>
2024-04-16 03:03:27 +0530

    Update problem6.js

--- project-root/problem6.js
+++ project-root/problem6.js
@@ -1,11 +1,7 @@
-export function filterBMWAndAudi(inventory) {
-    const BMWAndAudiCars = [];
-    for (let i = 0; i < inventory.length; i++) {
-        if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
-            BMWAndAudiCars.push(inventory[i]);
-        }
-    }
-    return BMWAndAudiCars;
+
+  export function filterBMWAndAudi(inventory) {
+    return inventory.filter(car => ['BMW', 'Audi'].includes(car.car_make));
 }
+
 
 
